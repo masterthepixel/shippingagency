c326395fd7573d80ed01bba2e136c48fb3628e5a
masterthepixel <45719243+masterthepixel@users.noreply.github.com>
2020-04-15 18:32:51 -0400

    New Menu

--- public/menuscript.js
+++ public/menuscript.js
@@ -1,7 +1,25 @@
-// https://codepen.io/breadz/pen/zrMRoo
+// https://codepen.io/fpecher/pen/BaNNMMb
 
 
-$('.toggle-menu').click(function () {
-  $(this).toggleClass('active');
-  $('#menu').toggleClass('open');
+let menuIcon = $('.toggle-nav');
+let nav = $('.nav');
+let navItem = $('.nav__item');
+
+menuIcon.click(function () {
+  $(this).toggleClass('toggle-nav--open');
+
+  if (nav.hasClass('nav--open')) {
+    navItem.removeClass('nav__item--open');
+
+    setTimeout(function () {
+      nav.removeClass('nav--open');
+    }, 1200);
+  }
+  else {
+    nav.addClass('nav--open');
+
+    setTimeout(function () {
+      navItem.addClass('nav__item--open');
+    }, 550);
+  }
 });
